b7da2824d51d8d8c9ac0bc5bb8688e17f1521eaa
namhokim <skaghzz@naver.com>
2021-09-01 10:06:24 +0900

    first commit

---
+++ src/main/java/com/example/springdockerdeployexample/SpringDockerDeployExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerdeployexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerDeployExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerDeployExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdockerdeployexample/SpringDockerDeployExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerdeployexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerDeployExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
